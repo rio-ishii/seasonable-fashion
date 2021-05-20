4a94c8be03c5a26cc2c7fc3be172e14786c0dc2b
rio.ishii <study.it.git@gmail.com>
2021-05-20 01:15:39 +0000

    add post image

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function add()
+  {
+      return view('posts.create');
+  }
+
+  public function create(Request $request)
+  {
+      $post = new Post;
+      $form = $request->all();
+
+      //s3アップロード開始
+      $image = $request->file('image');
+      // バケットの`myprefix`フォルダへアップロード
+      $path = Storage::disk('s3')->putFile('myprefix', $image, 'public');
+      // アップロードした画像のフルパスを取得
+      $post->image_path = Storage::disk('s3')->url($path);
+
+      $post->save();
+
+      return redirect('posts/create');
+}
+  
+public function index(Request $request)
+{
+    $posts = Post::all();
+
+    return view('posts.index', ['posts' => $posts]);
+}//
+}
---
+++ database/migrations/2021_05_20_094839_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            // 画像のパスを保存するカラムを追加
+            $table->string('image_path')->nullable();
+            $table->string('content');
+            $table->timestamps();
+            
+             // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
