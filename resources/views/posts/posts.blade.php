e0d9438a1fd3e9f5efa683acab085a9483dbe5d4
rio.ishii <study.it.git@gmail.com>
2021-05-27 05:53:02 +0000

    delete button

--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -9,7 +9,7 @@
                         <img src="{{ $post->image_path }}" class="img-fluid"><br>
                         <span class="text-muted">{{ $post->created_at }}</span>
                     </div></p>
-                    <div class="col-sm-6">
+                    <div class="col-sm-5">
                         <h5>Contents</h5>
                         <p class="mb-0">天気： {!! nl2br(e($post->weather)) !!}</p>
                         <p class="mb-0">最高気温： {!! nl2br(e($post->highTemperature)) !!}度</p>
@@ -17,12 +17,20 @@
                         <br>
                         <p class="mb-0"> {!! nl2br(e($post->content)) !!}</p>
                         <br>
+                    </div>
+                    <div class="col-sm-1">
+                            {{--{!! link_to_route('posts.detail', '編集する', ['post' => $post->id], ['class' => 'btn btn-light']) !!}--}}
+                        @if (Auth::id() == $user->id)
+                            <p><form action="{{ route('posts.delete',$post->id) }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit">
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                            </form></p>
+                        @else
                         
-                        {{--{!! link_to_route('posts.detail', '編集する', ['post' => $post->id], ['class' => 'btn btn-light']) !!}--}}
-            
-                        {!! Form::model($post, ['route' => ['posts.delete', $post->id], 'method' => 'delete']) !!}
-                            {!! Form::submit('削除する', ['class' => 'btn btn-light']) !!}
-                        {!! Form::close() !!}
+                        @endif
                     </div>
             </li>
             <hr noshade>
